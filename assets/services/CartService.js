887d9c1cf8be26d5a872658223b55db76c82ec75
evanh-shumakov <evanh.shumakov@gmail.com>
2024-10-02 19:57:30 +0300

    describe CartService with localStorage

---
+++ assets/services/CartService.js
@@ -0,0 +1,57 @@
+import { reactive } from 'vue';
+
+/**
+ * @typedef {Object} CartItem
+ * @property {number} id
+ * @property {string} name
+ * @property {string} description
+ * @property {string} image
+ * @property {float} price
+ */
+
+class CartService {
+    constructor() {
+        this._key = 'cart';
+        this._items = reactive(this._loadItems());
+    }
+
+    /**
+     * @return {CartItem[]}
+     */
+    getItems() {
+        return this._items;
+    }
+
+    /**
+     * @return float
+     */
+    getTotalPrice() {
+        return this._items.reduce((total, pizza) => total + pizza.price, 0).toFixed(2);
+    }
+
+    /**
+     * @param {CartItem} item
+     */
+    addItem(item) {
+        this._items.push(item)
+        this._saveItems(this.items);
+    }
+
+    clearCart() {
+        this._items.length = 0;
+    }
+
+    /**
+     * @return {CartItem[]}
+     */
+    _loadItems() {
+        const items = localStorage.getItem(this._key);
+        return items ? JSON.parse(items) : [];
+    }
+
+    _saveItems() {
+        localStorage.setItem(this._key, JSON.stringify(this._items));
+    }
+}
+
+export const cartService = new CartService();
