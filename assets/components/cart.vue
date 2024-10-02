8a90bc97274b2b598d192b814d4fbe5e779ff900
evanh-shumakov <evanh.shumakov@gmail.com>
2024-10-02 19:57:11 +0300

    extract the Cart component

---
+++ assets/components/cart.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="w-full max-w-4xl mt-12 mb-12">
+    <div class="bg-gray-900 p-6 rounded-lg shadow-md">
+      <p class="text-gray-400">В корзине пусто.</p>
+      <button class="mt-4 w-full bg-red-600 text-white py-2 rounded hover:bg-red-500">Перейти к оформлению</button>
+    </div>
+  </section>
+</template>
+
+<script>
+</script>
--- assets/pages/pizza/list.vue
+++ assets/pages/pizza/list.vue
@@ -4,12 +4,7 @@
     <p class="text-gray-400 text-sm mt-2">Где каждый кусок — шаг в бездну.</p>
   </header>
 
-  <section class="w-full max-w-4xl mt-12 mb-12">
-    <div class="bg-gray-900 p-6 rounded-lg shadow-md">
-      <p class="text-gray-400">В корзине пусто.</p>
-      <button class="mt-4 w-full bg-red-600 text-white py-2 rounded hover:bg-red-500">Перейти к оформлению</button>
-    </div>
-  </section>
+  <cart />
 
   <section class="w-full max-w-4xl mt-12">
     <h2 class="text-2xl text-gray-200 mb-6">Наше меню</h2>
