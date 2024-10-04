aa07330db9e01de891bb9d646dbb277ab50b100a
evanh-shumakov <evanh.shumakov@gmail.com>
2024-10-04 11:04:13 +0300

    vue: get correct grammatical number товар/товара/товаров

--- assets/components/cart.vue
+++ assets/components/cart.vue
@@ -2,7 +2,10 @@
   <section class="w-full max-w-4xl mt-12 mb-12 sticky top-0">
     <div class="bg-gray-900 p-6 rounded-lg shadow-md">
       <p v-if="isCartEmpty" class="text-gray-400">В корзине пусто.</p>
-      <p v-if="! isCartEmpty" class="text-gray-200">В корзине {{ items.length }} товаров на общую сумму ₽{{ totalPrice }}</p>
+      <p v-if="! isCartEmpty" class="text-gray-200">
+        В корзине {{ items.length }} {{ getProductCorrectGrammaticalNumber(items.length) }}
+        на общую сумму ₽{{ totalPrice }}
+      </p>
       <button v-if="! isCartEmpty" @click="clearCart" class="mt-4 w-full bg-red-600 text-white py-2 rounded hover:bg-red-500">Перейти к оформлению</button>
     </div>
   </section>
@@ -26,6 +29,14 @@
     methods: {
       clearCart() {
         cartService.clearCart();
+      },
+      /**
+       * @param {number} number
+       * @return {string}
+       */
+      getProductCorrectGrammaticalNumber(number) {
+        const rule = new Intl.PluralRules('ru-RU').select(number);
+        return {one: 'товар', few: 'товара', many: 'товаров'}[rule];
       },
     }
   }
